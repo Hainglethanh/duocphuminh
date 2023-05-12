74b712ae21d1655ed4a5f28f924b380b81b19095
Hai Nguyen <hai.nglethanh@gmail.com>
2023-05-12 15:25:12 +0700

    no message

--- frontend/src/components/router-head/router-head.tsx
+++ frontend/src/components/router-head/router-head.tsx
@@ -9,26 +9,12 @@ export const RouterHead = component$(() => {
   const loc = useLocation();
   return (
     <>
-      <title>
-        {head.title ||
-          "Dược Phú Minh - Giải pháp y tế hàng đầu cho người tiêu dùng"}
-      </title>
+      <title>{head.title || "Dược Phú Minh - Giải pháp y tế hàng đầu cho người tiêu dùng"}</title>
       <meta property="og:site_name" content="Dược Phú Minh" />
       <link rel="canonical" href={loc.url.href.replace("http:", "https:")} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Công ty cổ phần Dược Phú Minh" />
-      <meta
-        property="og:description"
-        content="Công ty cổ phần Dược Phú Minh - Niềm tin cho mọi nhà"
-      />
-      <meta
-        name="description"
-        content="Công ty cổ phần Dược Phú Minh - Niềm tin cho mọi nhà"
-      />
-      <meta
-        property="og:url"
-        content={loc.url.href.replace("http:", "https:")}
-      />
+      <meta property="og:url" content={loc.url.href.replace("http:", "https:")} />
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
